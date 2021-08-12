f939a3895e3717de40794dbdf8b49e37e464bacd
Hiroshy78910 <Angelhunter8253@gmail.com>
2021-08-12 16:57:17 -0300

    Exercios restAPI

---
+++ semana15/aula44-api/src/users.ts
@@ -0,0 +1,58 @@
+
+type User = {
+    id: number,
+    name: string,
+    email: string,
+    type: string,
+    age: number
+  }
+
+  enum USER_ROLES {
+      ADMIN = "ADMIN",
+      NORMAL = "NORMAL"
+  }
+
+export const users: User[] = [
+    {
+        id: 1,
+        name: "Alice",
+        email: "alice@email.com",
+        type: USER_ROLES.ADMIN,
+        age: 12
+    },
+    {
+        id: 2,
+        name: "Bob",
+        email: "bob@email.com",
+        type: USER_ROLES.NORMAL,
+        age: 36
+    },
+    {
+        id: 3,
+        name: "Coragem",
+        email: "coragem@email.com",
+        type: USER_ROLES.NORMAL,
+        age: 21
+    },
+    {
+        id: 4,
+        name: "Dory",
+        email: "dory@email.com",
+        type: USER_ROLES.NORMAL,
+        age: 17
+    },
+    {
+        id: 5,
+        name: "Elsa",
+        email: "elsa@email.com",
+        type: USER_ROLES.ADMIN,
+        age: 17
+    },
+    {
+        id: 6,
+        name: "Fred",
+        email: "fred@email.com",
+        type: USER_ROLES.ADMIN,
+        age: 60
+    }
+  ]
